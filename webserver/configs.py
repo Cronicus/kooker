a3193b9e6c62b0013aebf8c211839a016e434280
ANurag <anurag.kar.81190@gmail.com>
2017-09-03 02:37:27 +0530

    Information ralated to setting up of the database and users on mongodb, added to configs.py

--- webserver/configs.py
+++ webserver/configs.py
@@ -1,3 +1,40 @@
+# Setting up the users
+#
+# Open MongoDb shell on the terminal
+# > mongo
+#
+# Switch to the kooker database
+# >> use kooker
+#
+# Create the admin user
+# >> db.createUser(
+# >> {
+# >>   user: "topchef",
+# >>   pwd:  "cookin",
+# >>   roles: [ { role: "readWrite", db: "kooker" }]
+# >> }
+# >> )
+#
+# Create the generic user
+# >> db.createUser(
+# >> {
+# >>   user: "generic",
+# >>   pwd:  "generic",
+# >>   roles: [ { role: "read", db: "kooker" }]
+# >> }
+# >> )
+# >> exit
+#
+# Now edit the mongodb configuration file as root
+# > sudo nano /etc/mongod.conf
+#
+# Add these lines at the bottom to enable authentication
+# > security:
+# > 	authorization: enabled
+#
+# Now start the mongodb service
+# You are done!
+
 # Configuration options for kooker database
 
 HOSTNAME = '127.0.0.1'
